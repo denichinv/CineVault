e6ec7811dd4ebbb4c5f6725379c05e504990f589
Vilizar Denichin <denichinv@gmail.com>
2025-11-09 17:26:56 +0000

    MovieCard test created

---
+++ src/components/MovieCard.test.jsx
@@ -0,0 +1,11 @@
+import { render, screen } from "@testing-library/react";
+import MovieCard from "./MovieCard";
+
+describe("MovieCard component testing ", () => {
+  const mockMovie = {
+    id: 1062722,
+    poster_path: "/v9NLaLBbrkDwq44qG51v8T6sPuI.jpg",
+    vote_average: 7.785,
+    release_date: "2025-10-17",
+  };
+});
