f5b40491e33ae679aa41e0d2ca999531e656f450
Vilizar Denichin <denichinv@gmail.com>
2026-01-19 16:14:00 +0000

    add e2e tests for sorting options - rating and date

--- e2e/e2e/cinevault-sort.spec.ts
+++ e2e/e2e/cinevault-sort.spec.ts
@@ -10,7 +10,18 @@ test.describe("CineVault: user can apply sorting", () => {
       .getByRole("combobox")
       .nth(1)
       .selectOption({ value: "descending" });
-    const firstMovieTextAfter = await firstMovie.textContent();
     await expect(firstMovie).not.toHaveText(firstMovieTextBefore ?? "");
+  });
+  test("changing Popular to Rating/Date", async ({ page }) => {
+    await page.goto("https://cinevaultmoviesapp.netlify.app/");
+    const firstMovie = page.locator(".movie").first();
+    await expect(firstMovie).toBeVisible();
+    const firstMovieTextBefore = await firstMovie.textContent();
+    await page.getByRole("combobox").first().selectOption({ value: "date" });
+    await expect(firstMovie).not.toHaveText(firstMovieTextBefore ?? "");
+    const firstMovieTextAfterDate = await firstMovie.textContent();
+
+    await page.getByRole("combobox").first().selectOption({ value: "rating" });
+    await expect(firstMovie).not.toHaveText(firstMovieTextAfterDate ?? "");
   });
 });
